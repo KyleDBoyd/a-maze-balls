acbb3f25dabedb64646a64fd8cb17d2ae6fdbf12
Elliot <elliotwyman@gmail.com>
2014-12-15 23:29:19 -0800

    Added Mouse-based shooting

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ window.onload = function() {
     var SPEED = 500;
     var BOUNCE = 0.2;
 
+    var projectiles;
+    var nextFire;
+    var fireRate = 500;
+
     function preload () {
 
 	    game.load.image('back', 'res/tile-back.png');
     	game.load.spritesheet('link', 'res/link.png', 90, 90);
     	//game.load.spritesheet('fireball', 'res/fireball.png', 64, 64);
-    	game.load.spritesheet('orb', 'res/orb.png', 32, 32);
+    	game.load.spritesheet('projectile', 'res/orb.png', 32, 32);
 
 
     }
@@ -30,10 +34,6 @@ window.onload = function() {
 	 
 	    player = game.add.sprite(game.world.width/2, game.world.height/2, 'link');
 
-	    //Camera lock on the player
-	    game.camera.follow(player);
-    	game.camera.focusOnXY(0, 0);
-
 	    //Moving inside this Rectangle will not cause the camera to move.
     	//game.camera.deadzone = new Phaser.Rectangle(0, 0, 3000, 3000);
 
@@ -51,17 +51,34 @@ window.onload = function() {
 	  	player.animations.add('up', [10, 11, 12, 13, 14], 10, true);
 	    player.animations.add('down', [15, 16, 17, 18, 19], 10, true);
     
+	    //Camera lock on the player
+	    game.camera.follow(player);
+    	//game.camera.focusOnXY(player.body.position.x+45, player.body.position.y+45);
+
     	//  Reset the players velocity (movement)
-	    player.body.velocity.x = 0;
+	    player.body.velocity.x = player.body.velocity.y = 0;
+
+	    //Projectiles
+	    nextFire = game.time.now;
+	    projectiles = game.add.group();
+	    projectiles.enableBody = true;
+	    projectiles.physicsBodyType = Phaser.Physics.ARCADE;
+	    projectiles.createMultiple(30, 'projectile', 0, false);
+	    projectiles.setAll('anchor.x', 0.5);
+	    projectiles.setAll('anchor.y', 0.5);
+	    projectiles.setAll('checkWorldBounds', true);
 	    
     }
 
     function update() {
 
+    	//SWITCH to CURSORS
     	var keyboard = game.input.keyboard;
+    	var cursors = game.input.keyboard.createCursorKeys();
 
         if (keyboard.isDown(Phaser.Keyboard.LEFT))
 	    {
+	    	player.body.velocity.y = 0;
 	        //  Move to the left
 	        player.body.velocity.x = -SPEED;
 	 
@@ -69,6 +86,7 @@ window.onload = function() {
 	    }
 	    else if (keyboard.isDown(Phaser.Keyboard.RIGHT))
 	    {
+	    	player.body.velocity.y = 0;
 	        //  Move to the right
 	        player.body.velocity.x = SPEED;
 	 
@@ -76,6 +94,7 @@ window.onload = function() {
 	    }
 	    else if (keyboard.isDown(Phaser.Keyboard.UP))
 	    {
+	    	player.body.velocity.x = 0;
 	        //  Move to the right
 	        player.body.velocity.y = -SPEED;
 	 
@@ -83,6 +102,7 @@ window.onload = function() {
 	    }
 	    else if (keyboard.isDown(Phaser.Keyboard.DOWN))
 	    {
+	    	player.body.velocity.x = 0;
 	        //  Move to the right
 	        player.body.velocity.y = SPEED;
 	 
@@ -105,6 +125,25 @@ window.onload = function() {
 			orb.body.velocity.x = player.body.velocity.x;
 			orb.body.velocity.y = player.body.velocity.y;
 	    }
+
+	    if (game.input.activePointer.isDown)
+	    {
+	    	console.log("MOUSE PRESSED");
+	        fire();
+	    }
+
+	}
+
+	function fire () { 
+		
+		if (game.time.now > nextFire) 
+	    {
+	        nextFire = game.time.now;// + fireRate;
+	        var projectile = projectiles.getFirstExists(false);
+	        projectile.reset(player.x+45, player.y+45);
+	        projectile.rotation = game.physics.arcade.moveToPointer(projectile, 1000, game.input.activePointer, 500);
+		}
+
 	}
 
 };
